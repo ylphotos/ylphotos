957b9df6baeb9e987b4debe153f9c67eb14d138c
Yael Levey <yaellevey@gmail.com>
2013-11-04 16:54:09 +0000

    Adding functionality to Gallery

--- _site/js/gallery.js
+++ _site/js/gallery.js
@@ -1,13 +1,15 @@
-blueimp.Gallery(
-    document.getElementById('links'),
-    {
-        onslide: function (index, slide) {
-            var text = this.list[index].getAttribute('data-description'),
-                node = this.container.find('.description');
-            node.empty();
-            if (text) {
-                node[0].appendChild(document.createTextNode(text));
-            }
-        }
-    }
-);
+$('#blueimp-gallery').on('slide', function(event, index, slide) {
+	var gallery = $(this).data('gallery');
+	var text = gallery.list[index].getAttribute('data-description');
+	var node = $(slide).find('.description');
+	node.empty();
+	if (text) {
+		node.text(text);
+	}
+	var link = gallery.list[index].getAttribute('data-link');
+	var linkEl = $(slide).find('.link');
+	if (link) {
+		linkEl.attr('href', link);
+	}
+});
+
